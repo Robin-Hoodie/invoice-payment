a7e047808c10caa28e528965db0319b5ae84e640
Robin-Hoodie <robin@oreon.io>
2022-02-03 14:48:41 +0100

    feat: add support for adding and doubling compressed points

--- packages/backend/src/wallet/secp256k1-math.spec.ts
+++ packages/backend/src/wallet/secp256k1-math.spec.ts
@@ -1,9 +1,19 @@
 import { pointAdd as pointAddTinySecp256k1 } from "tiny-secp256k1";
 import { GENERATOR_POINT, pointDouble, pointAdd } from "./secp256k1-math";
 
+// We're using the tiny-secp256k1 to verify
+
+// TODO: uncompressed w/ uncompress function?
+export const GENERATOR_POINT_UNCOMPRESSED = Buffer.concat([
+  Buffer.from("02", "hex"),
+  GENERATOR_POINT.slice(1, 1 + 256 / 8),
+]);
+
 describe("Secp256k1 Mathematics", () => {
   it("should return the result of doubling a point as an uncompressed point", () => {
-    const pointDoubledActual = pointDouble(GENERATOR_POINT, false);
+    const pointDoubledActual = pointDouble(GENERATOR_POINT, {
+      compressed: false,
+    });
     const pointDoubledExpected = pointAddTinySecp256k1(
       GENERATOR_POINT,
       GENERATOR_POINT,
@@ -15,7 +25,9 @@ describe("Secp256k1 Mathematics", () => {
   });
 
   it("should return the result of doubling as a compressed point", () => {
-    const pointDoubledActual = pointDouble(GENERATOR_POINT, true);
+    const pointDoubledActual = pointDouble(GENERATOR_POINT, {
+      compressed: true,
+    });
     const pointDoubledExpected = pointAddTinySecp256k1(
       GENERATOR_POINT,
       GENERATOR_POINT,
@@ -26,10 +38,41 @@ describe("Secp256k1 Mathematics", () => {
     );
   });
 
+  it("should be able to handle doubling a compressed point and returning it as an uncompressed point", () => {
+    const pointDoubledActual = pointDouble(GENERATOR_POINT_UNCOMPRESSED, {
+      compressed: false,
+    });
+    const pointDoubledExpect = pointAddTinySecp256k1(
+      GENERATOR_POINT,
+      GENERATOR_POINT,
+      false
+    );
+    expect(pointDoubledActual.toString("hex")).toBe(
+      pointDoubledExpect.toString("hex")
+    );
+  });
+
+  it("should be able to handle doubling a compressed point and returning is as a compressed point", () => {
+    const pointDoubledActual = pointDouble(GENERATOR_POINT_UNCOMPRESSED, {
+      compressed: true,
+    });
+    const pointDoubledExpect = pointAddTinySecp256k1(
+      GENERATOR_POINT,
+      GENERATOR_POINT,
+      true
+    );
+    expect(pointDoubledActual.toString("hex")).toBe(
+      pointDoubledExpect.toString("hex")
+    );
+  });
+
   it("should return the sum of 2 points as an uncompressed point", () => {
-    const pointA = pointDouble(GENERATOR_POINT, false);
-    const pointB = pointDouble(pointDouble(GENERATOR_POINT, false), false);
-    const pointSumActual = pointAdd(pointA, pointB, false);
+    const pointA = pointDouble(GENERATOR_POINT, { compressed: false });
+    const pointB = pointDouble(
+      pointDouble(GENERATOR_POINT, { compressed: false }),
+      { compressed: false }
+    );
+    const pointSumActual = pointAdd(pointA, pointB, { compressed: false });
     const pointSumExpected = pointAddTinySecp256k1(pointA, pointB, false);
     expect(pointSumActual.toString("hex")).toBe(
       pointSumExpected.toString("hex")
@@ -37,9 +80,38 @@ describe("Secp256k1 Mathematics", () => {
   });
 
   it("should return the sum of 2 points as a compressed point", () => {
-    const pointA = pointDouble(GENERATOR_POINT, false);
-    const pointB = pointDouble(pointDouble(GENERATOR_POINT, false), false);
-    const pointSumActual = pointAdd(pointA, pointB, true);
+    const pointA = pointDouble(GENERATOR_POINT, { compressed: false });
+    const pointB = pointDouble(
+      pointDouble(GENERATOR_POINT, { compressed: false }),
+      { compressed: false }
+    );
+    const pointSumActual = pointAdd(pointA, pointB, { compressed: true });
+    const pointSumExpected = pointAddTinySecp256k1(pointA, pointB, true);
+    expect(pointSumActual.toString("hex")).toBe(
+      pointSumExpected.toString("hex")
+    );
+  });
+
+  it("should be able to handle adding two uncompressed points and returning the sum as an uncompressed point", () => {
+    const pointA = pointDouble(GENERATOR_POINT, { compressed: true });
+    const pointB = pointDouble(
+      pointDouble(GENERATOR_POINT, { compressed: true }),
+      { compressed: true }
+    );
+    const pointSumActual = pointAdd(pointA, pointB, { compressed: false });
+    const pointSumExpected = pointAddTinySecp256k1(pointA, pointB, false);
+    expect(pointSumActual.toString("hex")).toBe(
+      pointSumExpected.toString("hex")
+    );
+  });
+
+  it("should be able to handle adding two uncompressed point and returning the sum as a compressed point", () => {
+    const pointA = pointDouble(GENERATOR_POINT, { compressed: false });
+    const pointB = pointDouble(
+      pointDouble(GENERATOR_POINT, { compressed: false }),
+      { compressed: false }
+    );
+    const pointSumActual = pointAdd(pointA, pointB, { compressed: true });
     const pointSumExpected = pointAddTinySecp256k1(pointA, pointB, true);
     expect(pointSumActual.toString("hex")).toBe(
       pointSumExpected.toString("hex")
--- packages/backend/src/wallet/secp256k1-math.ts
+++ packages/backend/src/wallet/secp256k1-math.ts
@@ -1,9 +1,9 @@
-import { modInv } from "bigint-mod-arith";
+import { modInv, modPow } from "bigint-mod-arith";
 
 /**
- * A lot of the math and numbers here come from https://learnmeabitcoin.com/technical/ecdsa
- * These functions exist in other libraries (e.g. tiny-secp256k1), but are mostly reproduced in order for
- * the author to get a better grasp on the math behind Bitcoin.
+ * A lot of the math and numbers here come from https://learnmeabitcoin.com/technical/ecdsa & https://learnmeabitcoin.com/technical/public-key
+ * These functions exist in other libraries (e.g. tiny-secp256k1),
+ * but are mostly reproduced in order for the author to get a better grasp on the math behind Bitcoin.
  * As we use "bigint-mod-arith" and as noted in its README, the math used here is not constant time
  * and should thus not be used with private keys.
  **/
@@ -29,18 +29,23 @@ const GENERATOR_POINT_Y = Buffer.from(
   "483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8",
   "hex"
 );
-const GENERATOR_POINT_PREFIX = Buffer.from("04", "hex");
 
 export const GENERATOR_POINT = Buffer.concat([
-  GENERATOR_POINT_PREFIX,
+  Buffer.from("04", "hex"),
   GENERATOR_POINT_X,
   GENERATOR_POINT_Y,
 ]);
 
-const extractXAndYAsBigInt = (point: Uint8Array) => {
+const extractX = (point: Buffer) =>
+  BigInt(`0x${point.slice(1, 1 + 256 / 8).toString("hex")}`);
+
+const extractY = (point: Buffer) =>
+  BigInt(`0x${point.slice(1 + 256 / 8).toString("hex")}`);
+
+const extractCoordinates = (point: Buffer) => {
   return {
-    x: BigInt(`0x${Buffer.from(point.slice(1, 1 + 256 / 8)).toString("hex")}`),
-    y: BigInt(`0x${Buffer.from(point.slice(1 + 256 / 8)).toString("hex")}`),
+    x: extractX(point),
+    y: extractY(point),
   };
 };
 
@@ -51,14 +56,67 @@ const bigIntTo32ByteHex = (number: bigint) =>
  * This accounts for modulo of negative numbers, see below:
  * https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers#answer-17323608
  **/
-const moduloPrimeModulus = (number: bigint) => {
-  return ((number % PRIME_MODULUS) + PRIME_MODULUS) % PRIME_MODULUS;
+const modulo = (number: bigint, modulus = PRIME_MODULUS) => {
+  return ((number % modulus) + modulus) % modulus;
+};
+
+const pointIsCompressed = (point: Buffer) => {
+  const prefix = point.slice(0, 1).toString("hex");
+  if (prefix === "02" || prefix === "03") {
+    if (point.length === 264 / 8) {
+      return true;
+    }
+    throw new Error(
+      `Point ${point.toString(
+        "hex"
+      )} has the correct prefix (${prefix}) but does not have a length of 264 bits (${
+        point.length * 8
+      } bits)`
+    );
+  }
+  if (prefix === "04") {
+    if (point.length === 520 / 8) {
+      return false;
+    }
+    throw new Error(
+      `Point ${point.toString(
+        "hex"
+      )} has the correct prefix (${prefix}) but does not have a length of 520 bits (${
+        point.length * 8
+      })`
+    );
+  }
+  throw new Error(
+    `Point ${point.toString("hex")} does not have a '02', '03' or '04' prefix`
+  );
+};
+
+// See https://learnmeabitcoin.com/technical/public-key#how-to-decompress-a-public-key
+const uncompressPoint = (point: Buffer) => {
+  if (pointIsCompressed(point)) {
+    const prefix = point.slice(0, 1).toString("hex");
+    const x = extractX(point);
+    const ySquared = modulo(x ** 3n + 7n);
+    let y = modPow(ySquared, (PRIME_MODULUS + 1n) / 4n, PRIME_MODULUS);
+    if (prefix === "02" && modulo(y, 2n) !== 0n) {
+      y = modulo(PRIME_MODULUS - y);
+    }
+    if (prefix === "03" && modulo(y, 2n) === 0n) {
+      y = modulo(PRIME_MODULUS - y);
+    }
+    return Buffer.concat([
+      Buffer.from("04", "hex"),
+      Buffer.from(bigIntTo32ByteHex(x), "hex"),
+      Buffer.from(bigIntTo32ByteHex(y), "hex"),
+    ]);
+  }
+  return point;
 };
 
 const pointAsBuffer = (pointX: bigint, pointY: bigint, compressed: boolean) => {
   const pointXHex = bigIntTo32ByteHex(pointX);
   if (compressed) {
-    const pointYEven = pointY % 2n === 0n;
+    const pointYEven = modulo(pointY, 2n) === 0n;
     const prefix = pointYEven ? "02" : "03";
     return Buffer.from(`${prefix}${pointXHex}`, "hex");
   }
@@ -67,35 +125,36 @@ const pointAsBuffer = (pointX: bigint, pointY: bigint, compressed: boolean) => {
   return Buffer.from(`${prefix}${pointXHex}${pointYHex}`, "hex");
 };
 
-export const pointDouble = (point: Uint8Array, compressed = false) => {
-  const { x, y } = extractXAndYAsBigInt(point);
-  const slope = moduloPrimeModulus(
-    3n * x ** 2n * modInv(2n * y, PRIME_MODULUS)
-  );
-  const doubledPointX = moduloPrimeModulus(slope ** 2n - 2n * x);
-  const doubledPointY = moduloPrimeModulus(
-    (slope * (x - doubledPointX) - y) % PRIME_MODULUS
-  );
+export const pointDouble = (
+  point: Buffer,
+  { compressed } = { compressed: false }
+) => {
+  const pointUncompressed = uncompressPoint(point);
+  const { x, y } = extractCoordinates(pointUncompressed);
+  const slope = modulo(3n * x ** 2n * modInv(2n * y, PRIME_MODULUS));
+  const doubledPointX = modulo(slope ** 2n - 2n * x);
+  const doubledPointY = modulo(slope * (x - doubledPointX) - y);
   return pointAsBuffer(doubledPointX, doubledPointY, compressed);
 };
 
 export const pointAdd = (
-  pointOne: Uint8Array,
-  pointTwo: Uint8Array,
-  compressed = false
+  pointOne: Buffer,
+  pointTwo: Buffer,
+  { compressed } = { compressed: false }
 ) => {
   if (Buffer.from(pointOne).equals(Buffer.from(pointTwo))) {
-    return pointDouble(pointOne);
+    return pointDouble(pointOne, { compressed });
   }
-
-  const { x: pointOneX, y: pointOneY } = extractXAndYAsBigInt(pointOne);
-  const { x: pointTwoX, y: pointTwoY } = extractXAndYAsBigInt(pointTwo);
-  const slope = moduloPrimeModulus(
+  const pointOneUncompressed = uncompressPoint(pointOne);
+  const pointTwoUncompressed = uncompressPoint(pointTwo);
+  const { x: pointOneX, y: pointOneY } =
+    extractCoordinates(pointOneUncompressed);
+  const { x: pointTwoX, y: pointTwoY } =
+    extractCoordinates(pointTwoUncompressed);
+  const slope = modulo(
     (pointOneY - pointTwoY) * modInv(pointOneX - pointTwoX, PRIME_MODULUS)
   );
-  const sumPointX = moduloPrimeModulus(slope ** 2n - pointOneX - pointTwoX);
-  const sumPointY = moduloPrimeModulus(
-    slope * (pointOneX - sumPointX) - pointOneY
-  );
+  const sumPointX = modulo(slope ** 2n - pointOneX - pointTwoX);
+  const sumPointY = modulo(slope * (pointOneX - sumPointX) - pointOneY);
   return pointAsBuffer(sumPointX, sumPointY, compressed);
 };
