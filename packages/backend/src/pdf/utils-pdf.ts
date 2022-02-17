6e0ccf76f2da4c0b848118c4f52d30984daba317
Robin-Hoodie <robin@oreon.io>
2022-02-17 10:28:13 +0100

    feat: add utility for formatting a price for a specific currency

---
+++ packages/backend/src/pdf/utils-pdf.ts
@@ -0,0 +1,41 @@
+import jsPDF from "jspdf";
+import { CurrencyFiat } from "../types";
+import { currencyToSymbol } from "../utils";
+
+const round = (number: number, fractionDigits = 2) =>
+  Number(number.toFixed(fractionDigits));
+
+export const getOffsetsX = ({
+  internal: { pageSize },
+}: Pick<jsPDF, "internal">) => {
+  const docWidth = pageSize.getWidth();
+  return {
+    LEFT: round(docWidth * 0.1),
+    MIDDLE_LEFT: round(docWidth * 0.26),
+    MIDDLE: round(docWidth * 0.42),
+    MIDDLE_RIGHT: round(docWidth * 0.58),
+    RIGHT: round(docWidth * 0.74),
+  };
+};
+
+export const formatPriceForCurrency = (
+  price: number,
+  currency: CurrencyFiat
+) => {
+  const currencySymbol = currencyToSymbol(currency);
+  const fractionDigits = price.toFixed(2).slice(-2);
+  const priceFormattedWithoutFractionDigits = price
+    .toFixed(2)
+    .slice(0, -3)
+    .split("")
+    .reverse()
+    .reduce((priceFormatted, digit, i) => {
+      const insertComma = i === 0 ? false : i % 3 === 0;
+      return `${digit}${insertComma ? "," : ""}${priceFormatted}`;
+    }, "");
+  const priceFormatted = `${priceFormattedWithoutFractionDigits}.${fractionDigits}`;
+  if (currencySymbol === "kr") {
+    return `${priceFormatted} ${currencySymbol}`;
+  }
+  return `${currencySymbol}${priceFormatted}`;
+};
