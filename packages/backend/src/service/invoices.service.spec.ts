441b42d179825955e784a4d2c8fc7599783d954c
Robin-Hoodie <robin@oreon.io>
2022-02-24 07:50:50 +0100

    feat: add method to insert invoice to invoice service

--- packages/backend/src/service/invoices.service.spec.ts
+++ packages/backend/src/service/invoices.service.spec.ts
@@ -1,9 +1,16 @@
-import { getLatestInvoiceForYear } from "@/db/invoices.db";
-import { getNextInvoiceNumberForYear } from "@/service/invoices.service";
+import {
+  getLatestInvoiceForYear,
+  insertInvoice as insertInvoiceInDB,
+} from "@/db/invoices.db";
+import {
+  getNextInvoiceNumberForYear,
+  insertInvoice,
+} from "@/service/invoices.service";
 
 jest.mock("@/db/invoices.db");
 
 const mockedGetLatestInvoice = jest.mocked(getLatestInvoiceForYear);
+const mockedInsertInvoiceInDB = jest.mocked(insertInvoiceInDB);
 
 describe("Invoices Service", () => {
   it(`should return the next invoice number as '22002', when passed a date in the year 2022 
@@ -23,5 +30,15 @@ and no invoices yet existing for the year '22002'`, async () => {
     expect(await getNextInvoiceNumberForYear(new Date(2022, 6, 6))).toEqual(
       "#22001"
     );
+  });
+
+  it("should call 'insertInvoice' with '2' as the 'number' and the date of today", async () => {
+    const today = new Date();
+    await insertInvoice("#22002", today);
+    expect(mockedInsertInvoiceInDB).toHaveBeenCalledTimes(1);
+    expect(mockedInsertInvoiceInDB).toHaveBeenCalledWith({
+      number: 2,
+      date: today,
+    });
   });
 });
--- packages/backend/src/service/invoices.service.ts
+++ packages/backend/src/service/invoices.service.ts
@@ -1,9 +1,12 @@
-import { getLatestInvoiceForYear } from "@/db/invoices.db";
+import {
+  getLatestInvoiceForYear,
+  insertInvoice as insertInvoiceInDB,
+} from "@/db/invoices.db";
 
 export const getNextInvoiceNumberForYear = async (date: Date) => {
   const year = date.getFullYear();
-  const invoice = await getLatestInvoiceForYear(year);
   const yearPrefix = year.toString().slice(-2);
+  const invoice = await getLatestInvoiceForYear(year);
   if (invoice) {
     const invoiceNumberInYear = (invoice.number + 1)
       .toString()
@@ -11,4 +14,12 @@ export const getNextInvoiceNumberForYear = async (date: Date) => {
     return `#${yearPrefix}${invoiceNumberInYear}`;
   }
   return `#${yearPrefix}001`;
+};
+
+export const insertInvoice = async (invoiceNumber: string, date: Date) => {
+  const number = Number(invoiceNumber.slice(3)); // Drop '#' and prefix for year
+  return insertInvoiceInDB({
+    number,
+    date,
+  });
 };
