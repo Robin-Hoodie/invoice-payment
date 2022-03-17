951b78d6229d307b343a7a95899ad14de93368d4
Robin-Hoodie <robin@oreon.io>
2022-03-17 20:48:28 +0100

    feat: add code for decoding transactions

---
+++ packages/backend/src/wallet/types-transaction.ts
@@ -0,0 +1,28 @@
+interface Input {
+  txid: string;
+  vout: number;
+  scriptSig: string;
+  sequence: string;
+}
+
+export interface InputWithIndexEnd {
+  input: Input;
+  indexEnd: number;
+}
+
+interface Output {
+  value: number;
+  scriptPubKey: string;
+}
+
+export interface OutputWithIndexEnd {
+  output: Output;
+  indexEnd: number;
+}
+
+export interface Transaction {
+  version: number;
+  inputs: Input[];
+  outputs: Output[];
+  lockTime: number;
+}
